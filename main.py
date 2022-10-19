b0207a91d6f5d9e6b1b2ef69f6cea1ae5a3a764f
Maria Pallares <101284724+marpaldie@users.noreply.github.com>
2022-10-19 13:15:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+
+
+def ejecutar_iqtree(directorio_bin, fichero1, fichero2,numerito_nose_que_es,modo = MF+MERGE):
+    result = []
+
+    # Bin\iqtree -s Lithobates_concat.txt - q Lithobates_part.txt -m MF+MERGE -alrt 1000
+    comando = [directorio_bin,
+               '-s',
+               fichero1,
+               '-q',
+               fichero2,
+               '-m',
+               modo,
+               '-alrt',
+               str(numerito_nose_que_es)
+               ]
+    print("Esto debe de coincidir con lo que metias tu a mano",' '.join(comando))
+    process = subprocess.Popen(comando,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    for line in process.stdout:
+        result.append(line)
+    errcode = process.returncode
+    for line in result:
+        print(line.decode('utf-8'))
+    if errcode is not None:
+        raise Exception('cmd %s failed, see above for details', errcode)
+
+if __name__ == '__main__': #Esto se utiliza formalmente para la parte de ejecución, para que este código no se ejecute si se importa este fichero
+
+    ruta_comando = ".Bin/iqtree" #mete aqui la ruta entera, así no tienes que hacer el cd.
+    # directorio_ficheros= 'C:/blabla' #donde tienes los datos
+
+    #mejora los nombres
+    fichero1 = "Lithobates_concat.txt"
+    fichero2 = "Lithobates_part.txt"
+
+    #alomejor tienes que meter la ruta entera. Utiliza os.path.join para formar las rutas.
+    # path_fichero_1 = os.path.join(directorio_ficheros, fichero1)
+    # path_fichero_2 = os.path.join(directorio_ficheros, fichero2)
+
+    numerito_nose_que_es = 1000
+
+    ejecutar_iqtree(ruta_comando, fichero1,fichero2,numerito_nose_que_es)
+
